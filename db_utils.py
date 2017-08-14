7e7bb8deff0f2c1ca3268996860afd1752f4bcf4
Amir Keren <amirkeren3@gmail.com>
2017-08-14 22:04:29 +0300

    Update db_utils.py

--- db_utils.py
+++ db_utils.py
@@ -16,95 +16,99 @@ DB_PASSWORD = config.get('DatabaseSection', 'database.password');
 DB_NAME = 'pingpong'
 TABLE_NAME = 'sessions'
 
+def print_log(msg):
+    st = datetime.datetime.fromtimestamp(time.time()).strftime(TIMESTAMP_FORMAT)
+    print st, msg
+
 def connect_to_db():
-    print 'connecting to db'
+    print_log('connecting to db')
     conn = MySQLdb.connect(host=DB_HOST, user=DB_USER, passwd=DB_PASSWORD, db=DB_NAME)
-    print 'connected'
+    print_log('connected')
     return conn
 
 def is_open_session_exists():
-    print 'checking if an open session exists'
+    print_log('checking if an open session exists')
     open_session_found = False
     conn = connect_to_db()
     x = conn.cursor()
     x.execute('SELECT id FROM ' + TABLE_NAME + ' WHERE end = %s', (DEFAULT_TIMESTAMP))
     data = x.fetchall()
     if (len(data) > 0):
-        print 'found an open session'
+        print_log('found an open session')
         open_session_found = True
     else:
-        print 'no open session found'
+        print_log('no open session found')
     x.close()
     conn.close()
     return open_session_found
 
 def handle_motion_detected():
-    print 'motion detected!'
+    print_log('motion detected!')
     conn = connect_to_db()
     x = conn.cursor()
-    print 'checking open sessions'
+    print_log('checking open sessions')
     x.execute('SELECT id FROM ' + TABLE_NAME + ' WHERE end = %s', (DEFAULT_TIMESTAMP))
     data = x.fetchall()
     if (len(data) > 0):
         session_id = data[0][0]
-        print 'updating session', session_id, 'in progress'
+        print_log('updating session', session_id, 'in progress')
         update_session(conn, x, session_id)
     else:
-        print 'no open sessions found, adding new session'
+        print_log('no open sessions found, adding new session')
         add_new_session(conn, x)
     x.close()
     conn.close()
     
 def handle_no_motion(DETECTION_FRAME_LENGTH_IN_SECONDS, MAX_SESSION_TIME_IN_SECONDS):
-    print 'no motion detected for', DETECTION_FRAME_LENGTH_IN_SECONDS, 'seconds'
+    print_log('no motion detected for', DETECTION_FRAME_LENGTH_IN_SECONDS, 'seconds')
     conn = connect_to_db()
     x = conn.cursor()
-    print 'checking open sessions'
+    print_log('checking open sessions')
     x.execute('SELECT id, unix_timestamp(last_updated) FROM ' + TABLE_NAME + ' WHERE end = %s', (DEFAULT_TIMESTAMP))
     data = x.fetchall()
     if (len(data) > 0):
         session_id = data[0][0]
         last_updated = data[0][1]
-        print 'session', session_id, 'in progress, checking if it needs to end'
+        print_log('session', session_id, 'in progress, checking if it needs to end')
         diff = time.time() - last_updated
         if (diff > MAX_SESSION_TIME_IN_SECONDS):
             close_session(conn, x, session_id)
         else:
-            print 'session', session_id, 'did not time out yet'
+            print_log('session', session_id, 'did not time out yet')
     else:
-        print 'no open sessions'
+        print_log('no open sessions')
     x.close()
     conn.close()
 
 def clean_open_sessions(conn, x):
-    print 'cleaning open sessions'
+    print_log('cleaning open sessions')
     x.execute('DELETE FROM ' + TABLE_NAME + ' WHERE end = %s', (DEFAULT_TIMESTAMP))
     conn.commit()
-    print 'cleaned open sessions'
+    print_log('cleaned open sessions')
     
 def close_session(conn, x, session_id):
-    print 'closing session', session_id
+    print_log('closing session', session_id)
     ts = time.time()
     timestamp = datetime.datetime.fromtimestamp(ts).strftime(TIMESTAMP_FORMAT)
     x.execute('UPDATE ' + TABLE_NAME + ' SET end = %s WHERE id = %s', (timestamp, session_id))
     conn.commit()
-    print 'closed session', session_id
+    print_log('closed session', session_id)
     
 def update_session(conn, x, session_id):
-    print 'updating session', session_id
+    print_log('updating session', session_id)
     ts = time.time()
     timestamp = datetime.datetime.fromtimestamp(ts).strftime(TIMESTAMP_FORMAT)
     x.execute('UPDATE ' + TABLE_NAME + ' SET last_updated = %s WHERE id = %s', (timestamp, session_id))
     conn.commit()
-    print 'updated session', session_id
+    print_log('updated session', session_id)
     
 def add_new_session(conn, x):
-    print 'adding new session'
+    print_log('adding new session')
     ts = time.time()
     timestamp = datetime.datetime.fromtimestamp(ts).strftime(TIMESTAMP_FORMAT)
     x.execute('INSERT INTO ' + TABLE_NAME + ' (last_updated) VALUES (%s)', (timestamp))
     conn.commit()
-    print 'added new session'
+    print_log('added new session')
 
 def setup_database():
     conn = connect_to_db()
