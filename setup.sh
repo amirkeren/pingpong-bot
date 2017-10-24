39b502ada4bee80aaf88f36d685b73b80d96f553
amir.k <amir.k@taboola.com>
2017-10-24 11:37:07 +0300

    add setup script and config file

---
+++ setup.sh
@@ -0,0 +1,5 @@
+sudo apt-get update && sudo apt-get upgrade
+sudo apt-get install mysql-server --fix-missing
+sudo apt-get install libmysqlclient-dev
+sudo apt-get install python-mysqldb
+sudo pip install slackclient
